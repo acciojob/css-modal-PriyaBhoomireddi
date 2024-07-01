6d1d0e469a54b2c2ddff19b587e103976a37368e
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-01 21:53:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+      document.getElementById('openModal').onclick = function(){
+        document.getElementById('myModal').style.display = "block";
+      }
+      
+      document.querySelector('.close-modal').onclick = function(){
+        document.getElementById('myModal').style.display = "none";
+      }
+      
+      window.onclick = function(event){
+        var modal =  document.getElementById('myModal');
+        if(event.target == modal){
+          modal.style.display = "none";
+        }
+      }
